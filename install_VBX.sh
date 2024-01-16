30f902a9238a5adee930c707fd3abbb9af27e00c
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-01-16 19:47:26 +0100

    Update install_VBX.sh

--- install_VBX.sh
+++ install_VBX.sh
@@ -3,14 +3,11 @@
 # Désinstaller virtualbox-dkms
 apt-get purge virtualbox-dkms
 
-# Si la commande précédente ne fonctionne pas, essayez :
-dpkg -P virtualbox-dkms
-
 # Désinstaller virtualbox
 apt-get purge virtualbox
 
 # Installer la dernière version de VirtualBox 
-dpkg -i virtualbox-7.0_7.0.12-159484~Ubuntu~jammy_amd64.deb
+dpkg -i [Ubuntu 22.04]virtualbox-7.0Ubuntu~jammy_amd64.deb
 
 # Mettre à jour les dépôts et installer les dépendances manquantes
 apt install -y
