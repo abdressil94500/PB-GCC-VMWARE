e1b223477b02eddba26dc2734436108065abfa84
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-01-16 19:44:53 +0100

    Update install_VMWare.sh

--- install_VMWare.sh
+++ install_VMWare.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Désinstaller VMware Workstation (si nécessaire)
+# Désinstaller VMware Workstation 
 vmware-installer --uninstall-product vmware-workstation
 
 # Installer GCC 12
 apt-get install gcc-12
+
+# Rendre exécutable le fichier bundle
+chmod +x ./[Ubuntu]VMware-Workstation-Full-17.0.2.bundle
+#Lancer l'installation
+./[Ubuntu]VMware-Workstation-Full-17.0.2.bundle
 
 # Cloner le dépôt vmware-host-modules
 git clone https://github.com/mkubecek/vmware-host-modules.git
